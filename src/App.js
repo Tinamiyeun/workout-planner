64097b6ab69a402093c4ff2151091210b975cb60
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-06-17 09:30:45 -0400

    register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import { CustomizePlan } from './pages/CustomizePlan';
 import {Route, Routes} from 'react-router-dom';
 import {Workout} from './pages/Workout';
+import { Register } from 'pages/Register';
+import React, {useState} from 'react';
+
+
+
+export const VipContext = React.createContext({
+  isVip: false,
+  setIsVip: () =>{},
+});
+
 
 function App() {
+
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+  const loggedInValueToProvide = [isLoggedIn, setIsLoggedIn];
+
+  const [isVip, setIsVip] = useState(false);
+  const vipToProvide = [isVip, setIsVip];
+
   return (
     <Routes>
       <Route path="/customizeplan" element={<CustomizePlan />} />
       <Route path="/workout" element={<Workout />} />
+      <Route path="/register" element={<Register />} />
+
     </Routes>
   );
 }
