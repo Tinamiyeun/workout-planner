d14b4a71e5cececa69e9f6be7ac0602a2682d6ef
carolinn_lyy <57008592+carolinn-lyy@users.noreply.github.com>
2022-06-17 19:33:15 -0400

    comment.js added

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,14 +1,41 @@
-import Button from 'react-bootstrap';
+import {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
 
 function Comment(){
 
-    return (
-        <>
-            <Button>Too Easy</Button>
-            <Button>Great</Button>
-            <Button>Too Hard</Button>
-        </>
+    const [comment, setComment] = useState('');
 
+    const tooEasy = () =>{
+        setComment('too easy');
+    };
+
+    const great =() => {
+        setComment('great');
+    }
+
+    const tooHard =() => {
+        setComment('too hard');
+    }
+
+    return (
+        <Container>
+            <Card className="text-center">
+                <Card.Body>
+                    <Card.Text>What do you think about today's workout?</Card.Text>
+                    <Row className="justify-content-md-center">
+                        <Col><Button onClick={tooEasy}>Too Easy</Button></Col>
+                        <Col><Button onClick={great}>Great</Button></Col>
+                        <Col><Button onClick={tooHard}>Too Hard</Button></Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+        </Container>
+        
     )
 }
 
