ea98813781df14e1147972d96740d07fa7b2d284
carolinn_lyy <57008592+carolinn-lyy@users.noreply.github.com>
2022-06-17 19:13:52 -0400

    comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,4 @@
-import useState from 'react';
+import {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -8,35 +8,35 @@ import Card from 'react-bootstrap/Card';
 
 function Comment(){
 
-    // const [comment, setComment] = useState('');
+    const [comment, setComment] = useState('');
 
-    // const tooEasy = () =>{
-    //     setComment('too easy');
-    // };
+    const tooEasy = () =>{
+        setComment('too easy');
+    };
 
-    // const great =() => {
-    //     setComment('great');
-    // }
+    const great =() => {
+        setComment('great');
+    }
 
-    // const tooHard =() => {
-    //     setComment('too hard');
-    // }
+    const tooHard =() => {
+        setComment('too hard');
+    }
 
-    // return (
-    //     <Container>
-    //         <Card className="text-center">
-    //             <Card.Body>
-    //                 <Card.Text>What do you think about today's workout?</Card.Text>
-    //                 <Row className="justify-content-md-center">
-    //                     <Col><Button onClick={tooEasy}>Too Easy</Button></Col>
-    //                     <Col><Button onClick={great}>Great</Button></Col>
-    //                     <Col><Button onClick={tooHard}>Too Hard</Button></Col>
-    //                 </Row>
-    //             </Card.Body>
-    //         </Card>
-    //     </Container>
+    return (
+        <Container>
+            <Card className="text-center">
+                <Card.Body>
+                    <Card.Text>What do you think about today's workout?</Card.Text>
+                    <Row className="justify-content-md-center">
+                        <Col><Button onClick={tooEasy}>Too Easy</Button></Col>
+                        <Col><Button onClick={great}>Great</Button></Col>
+                        <Col><Button onClick={tooHard}>Too Hard</Button></Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+        </Container>
         
-    // )
+    )
 }
 
 export default Comment;
