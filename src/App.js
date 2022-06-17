3e9f411fd1eb76f876840d7ef6372647f91c4f23
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-06-17 16:04:36 -0400

    route changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { Route, Routes} from 'react-router-dom';
+import PageLayout from 'layouts/PageLayout';
+import HomePage from 'pages/HomePage';
+import AboutUs from './pages/AboutUs';
 import { CustomizePlan } from './pages/CustomizePlan';
-import {Route, Routes} from 'react-router-dom';
-import {Workout} from './pages/Workout';
+import { Workout } from 'pages/Workout';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/customizeplan" element={<CustomizePlan />} />
-      <Route path="/workout" element={<Workout />} />
-    </Routes>
-  );
+
+return (
+<>
+<Routes>
+<Route path="/" element={<PageLayout />}>
+<Route index element={<HomePage />} />
+<Route path="about" element={<AboutUs />} />
+<Route path="customizeplan" element={<CustomizePlan />} />
+<Route path="workout" element={<Workout />} />
+
+</Route>
+</Routes>
+</>
+
+);
+
 }
+
+
 
 export default App;
