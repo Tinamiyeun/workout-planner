4d9e3cbce8497e9ebcfcdb971cefb5acf8a47d49
carolinn_lyy <57008592+carolinn-lyy@users.noreply.github.com>
2022-06-18 12:51:08 -0400

    mode change parially finished, text color need some adjust later

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import AboutUs from './pages/AboutUs';
 import { CustomizePlan } from './pages/CustomizePlan';
 import {Workout} from './pages/Workout';
 import { Register } from 'pages/Register';
-import React, {useState} from 'react';
+import React, {createContext, useState} from 'react';
 import { Login } from 'pages/Login';
 import { VipContext } from 'contexts/VipContext';
 import { UserNameContext } from 'contexts/UserNameContext';
 import PostWorkout from './pages/PostWorkout';
+import History from 'pages/History';
+import ReactSwitch from 'react-switch';
+import { lightTheme, darkTheme } from 'components/Theme';
+import { GlobalStyles } from 'components/Global';
+import { ThemeProvider } from 'styled-components';
+
+// import { ThemeContext } from 'styled-components';
+
+export const ThemeContext = createContext(null);
 
 export const LoggedInContext = React.createContext({
   isLoggedIn:false,
@@ -18,6 +27,20 @@ export const LoggedInContext = React.createContext({
 
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () =>{
+    if(theme === "light"){
+      setTheme('dark')
+    }else{
+      setTheme('light');
+    }
+  }
+  React.useEffect(() => {
+
+    console.log({theme})
+    
+    })
 
   const [isLoggedIn,setIsLoggedIn] = useState(false);
   const loggedInValueToProvide = [isLoggedIn, setIsLoggedIn];
@@ -29,25 +52,34 @@ function App() {
   const userNameToProvide = [userName, setUserName];
 
   return (
-<>
+    <ThemeProvider theme={theme ==="light" ? lightTheme : darkTheme}>
+      <>
+      <GlobalStyles />
+      <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
     <LoggedInContext.Provider value = {loggedInValueToProvide}>
       <VipContext.Provider value = {vipToProvide}>
         <UserNameContext.Provider value = {userNameToProvide}>
+        
         <Routes>
           <Route path="/" element={<PageLayout />}>
             <Route index element={<HomePage />} />
             <Route path="about" element={<AboutUs />} />
             <Route path="customizeplan" element={<CustomizePlan />} />
             <Route path="workout" element={<Workout />} />
+            <Route path="postworkout" element={<PostWorkout />} />
+            <Route path="history" element={<History />} />
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
-            <Route path="postworkout" element={<PostWorkout />} />
           </Route>
         </Routes>
         </UserNameContext.Provider>
       </VipContext.Provider>
     </LoggedInContext.Provider>
-</>
+    </>
+      {/* </div> */}
+
+    </ThemeProvider>
+
   );
   }
 
---
+++ src/components/Theme.js
@@ -0,0 +1,12 @@
+
+export const lightTheme = {
+    body: '#FFFFFF',
+    toggleBorder: '#FFF',
+    text: '#363537',
+}
+
+export const darkTheme = {
+    body: '#363537',
+    toggleBorder: '#6B8096',
+    text: '#FAFAFA',
+}
