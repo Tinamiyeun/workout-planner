7b1ded9c72207384f68644fe8734de6f10d88d6e
carolinn_lyy <57008592+carolinn-lyy@users.noreply.github.com>
2022-06-18 14:51:32 -0400

    theme change finalized version

--- src/components/Global.js
+++ src/components/Global.js
@@ -8,7 +8,7 @@ box-sizing: border-box;
 }
 body {
     background-color: ${({ theme }) => theme.body};
-    foreground-color: ${({ theme }) => theme.text}
+    color: ${({ theme }) => theme.color};
     font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
     transition: all 0.25s linear;
 }
--- src/components/Theme.js
+++ src/components/Theme.js
@@ -2,11 +2,11 @@
 export const lightTheme = {
     body: '#FFFFFF',
     toggleBorder: '#FFF',
-    text: '#363537',
+    color: '#363537',
 }
 
 export const darkTheme = {
     body: '#363537',
     toggleBorder: '#6B8096',
-    text: '#a98004',
+    color: '#a98004',
 }
