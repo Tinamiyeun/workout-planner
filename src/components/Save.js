55d85a39391b9c69fe5465a63f4777d5e771274a
carolinn_lyy <57008592+carolinn-lyy@users.noreply.github.com>
2022-06-17 21:10:06 -0400

    save.js changes

--- src/components/Save.js
+++ src/components/Save.js
@@ -1,9 +1,33 @@
 import Button from 'react-bootstrap/Button';
+import {useState} from 'react';
 
 function Save() {
+    
+    const [savedPlan, setSavedPlan] = useState('');
+    const yourPlan = window.sessionStorage.getItem('yourPlan');
+    console.log(yourPlan);
+
+    // setSavedPlan(yourPlan);
+
+    const handleOnClick = (event) => {
+        fetch("http://localhost:3001/history", {
+        method: "POST",
+        body: JSON.stringify({//pass the value of input field here
+          
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((data) => data.json())
+        .then((json) => {
+            
+        });
+    }
+    
 
     return (
-        <Button>Save</Button>
+        <Button onClick={handleOnClick}>Save</Button>
     )
 }
 
