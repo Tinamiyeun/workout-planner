10bbf4070ca3e5fb2b707bd4ee46f84302fe841d
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-06-18 23:28:16 -0400

    header changes

--- src/components/useFetchAPI.js
+++ src/components/useFetchAPI.js
@@ -4,7 +4,7 @@ export const options = {
   method: 'GET',
   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    'X-RapidAPI-Key': '082a5f6ba3mshd277027b79d445bp15ed55jsnb7c2a88adc19',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
