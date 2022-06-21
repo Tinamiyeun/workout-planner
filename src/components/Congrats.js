7f7b8d60fedd4afe4029afa147fc19146f4d0400
carolinn_lyy <57008592+carolinn-lyy@users.noreply.github.com>
2022-06-21 18:37:06 -0400

    confetti added

--- src/components/Congrats.js
+++ src/components/Congrats.js
@@ -1,19 +1,34 @@
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import {useContext} from 'react';
+import {useContext, useState, useEffect, useRef} from 'react';
 import { UserNameContext } from 'contexts/UserNameContext';
+import Confetti from 'react-confetti'
 
 function Congrats() {
 
     const [userName, setUserName] = useContext(UserNameContext);
+
+    const[height, setHeight] = useState();
+    const[width, setWidth] = useState();
+    const confettiRef = useRef();
+
+    useEffect(() =>{
+        setHeight(confettiRef.current.Height);
+        setWidth(confettiRef.current.Width);
+    }, [])
+
     return (
         <Container>
-            <Card className="text-center">
+            <Card className="text-center" ref={confettiRef}>
                 <Card.Body>
                     <Card.Title><h2>{userName}, Well Done!</h2></Card.Title>
                     <Card.Text>A step closer to your goal!</Card.Text>
                 </Card.Body>
             </Card>
+            <Confetti
+                numberOfPieces={80}
+                width={width}
+                height={height} />
         </Container>
     )
 }
